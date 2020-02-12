5c610417d2a94898c5450db46fabba90d800a870
Kyle Talmage <ktalmage85@gmail.com>
2020-02-12 19:10:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
  
  if array.count < 3
    return array.join(" and ")
-  else
+  elsif array.count > 3
   return array.first(2).join(", ") + ", and " + array.last
+else
   end   
 end
