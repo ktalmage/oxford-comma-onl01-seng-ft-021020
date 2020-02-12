f01e3af570ae225a6bc8b43473c3108a052f270c
Kyle Talmage <ktalmage85@gmail.com>
2020-02-12 18:32:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
   array.to_sentence
 end
-array = ["fiddleheads","okra","kohlrabi"]
-puts oxford_comma(array)
+#array = ["fiddleheads","okra","kohlrabi"]
+#puts oxford_comma(array)
