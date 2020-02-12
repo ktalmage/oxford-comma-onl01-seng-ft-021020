27480b49af3f31c59575451c79b9df8b33e485f4
Kyle Talmage <ktalmage85@gmail.com>
2020-02-12 18:05:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  if array.count > 1
-    array.join(",")
-  elsif array.count > 2
-    array.join()
+  array.each do |line|
+    array = line.split(",")
+    print array.join(",") << String(array.length)
+  end
