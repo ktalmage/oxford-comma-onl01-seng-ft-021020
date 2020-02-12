b95c6ef750ab860099795340431a214503070253
Kyle Talmage <ktalmage85@gmail.com>
2020-02-12 19:26:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
  if array.count == 3
    return array.first(2).join(", ") + ", and " + array.last
   elsif array.count > 3
-  return array[0..-2].first.join(", ") + ", and " + array[-1]
+  return array[0..-2].join(", ") + ", and " + array[-1]
   else
   return array.join(" and ")
   end   
